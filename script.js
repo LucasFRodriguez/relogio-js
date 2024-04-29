e441645d962a84d7e2c905556c498093b1fded87
LucasFRodriguez <lucasfrz26@gmail.com>
2024-04-29 14:21:46 -0300

    finalizado

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let minutos = document.getElementById('minutos');
 let segundos = document.getElementById('segundos');
 let ampm = document.getElementById('ampm');
 
+let hh = document.getElementById('hh');
+let mm = document.getElementById('mm');
+let ss = document.getElementById('ss');
+
+let hr_ponto = document.querySelector('.hr_ponto');
+let min_ponto = document.querySelector('.min_ponto');
+let seg_ponto = document.querySelector('.seg_ponto');
+
+
 setInterval(()=>{
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     let amdDmd = h >= 12 ? 'PM' : 'AM';
-    
-    
     
     // CONVERTENDO 24HRS PARA 12HRS
     
@@ -27,5 +34,24 @@ setInterval(()=>{
     minutos.innerHTML = m + '<br><span>Minutos</span>';
     segundos.innerHTML = s + '<br><span>Segundos</span>';
     ampm.innerHTML = amdDmd;
+
+
+
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+    // 360 / 12 = 30
+    hr_ponto.style.transform = `rotate(${h * 30}deg)` ;
+
+    // 360 / 60 = 6
+    min_ponto.style.transform = `rotate(${m * 6}deg)` ;
+
+    // 360 / 60 = 6
+    seg_ponto.style.transform = `rotate(${s * 6}deg)` ;
+
+
+
 
 });
